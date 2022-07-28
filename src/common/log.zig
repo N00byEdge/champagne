11fff37c1dc3dffeb63e956dc70143df29721ff3
Hannes Bredberg <hannesbredberg@gmail.com>
2022-07-28 21:52:27 +0200

    Lock on logging

--- src/common/log.zig
+++ src/common/log.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
 
+var log_lock = std.Thread.Mutex{};
+
 pub fn scoped(comptime tag: anytype) fn (comptime fmt: []const u8, args: anytype) callconv(.Inline) void {
     return struct {
         pub inline fn f(comptime fmt: []const u8, args: anytype) void {
+            log_lock.lock();
+            defer log_lock.unlock();
             std.debug.print(@tagName(tag) ++ ": " ++ fmt ++ "\n", args);
         }
     }.f;
